fc32847ff9653a63fb097bfcdc9e1b222bb15859
Teju0902 <tiammu519@gmail.com>
2026-06-11 22:17:21 +0530

    Create script.sh

---
+++ Fullstack/commandline/Bicycle_word_II/script.sh
@@ -0,0 +1,48 @@
+# 1
+pwd
+
+# 2
+ls
+
+# 3
+cd freight
+
+# 4
+ls
+
+# 5
+cd porteur
+
+# 6
+cd ../..
+ls
+
+# 7
+cd mountain/downhill
+
+# 8
+touch dirt.txt
+
+# 9
+touch mud.txt
+
+# 10
+ls
+
+# 11
+mkdir safety
+
+# 12
+cd ../..
+
+# 13
+ls
+
+# 14
+mkdir bmx
+
+# 15
+touch bmx/tricks.txt
+
+# 16
+ls
