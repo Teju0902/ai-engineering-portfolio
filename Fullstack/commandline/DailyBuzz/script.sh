0ffafba4d286415a2011b3e3b6ccb0cf4b13968c
Teju0902 <tiammu519@gmail.com>
2026-06-11 22:31:31 +0530

    Create script.sh

---
+++ Fullstack/commandline/DailyBuzz/script.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Daily Buzz Project
+
+# Task 1 & 2
+pwd
+ls
+
+# Task 3
+cd national/politics
+
+# Task 4
+ls
+
+# Task 5
+mkdir elections
+
+# Task 6
+cd elections
+
+# Task 7
+touch candidates.txt issues.txt
+
+# Task 8
+cd ../../..
+pwd
+
+# Task 9
+mkdir business
+cd business
+
+# Task 10
+ls
+
+# Task 11
+mkdir startups
+cd startups
+
+# Task 12
+cd ..
+
+# Task 13
+mkdir startups/disruptors
+
+# Task 14
+touch startups/disruptors/tech.txt
+touch startups/disruptors/design.txt
+touch startups/disruptors/education.txt
+
+# Task 15
+cd ..
+pwd
